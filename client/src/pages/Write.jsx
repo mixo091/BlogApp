a6bdfbfe23ee5a8a5e4749ca92d723a34723b970
mixo091 <mmichopoulos1.1@gmail.com>
2023-03-23 16:44:28 +0200

    Added rich text editor

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import  { useState } from 'react';
 import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function Write() {
 
   const [value, setValue] = useState('');
-  
+
   return (
     <div className='add'>
       <div className="content">
